f81d4ad5d32eedf8e9d50c9789e73698302d8ca6
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-09-24 13:27:44 +0200

    Run build

---
+++ docs/precache-manifest.d34de0076a377b502f1d48f7c96aec80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f30287176f7de6e6c84b434a3b484e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e3486199ca017dde726a",
+    "url": "./static/css/main.009f1622.chunk.css"
+  },
+  {
+    "revision": "298a1fbf6e284ed081e4",
+    "url": "./static/js/2.c7703ecf.chunk.js"
+  },
+  {
+    "revision": "e3486199ca017dde726a",
+    "url": "./static/js/main.a3edf791.chunk.js"
+  },
+  {
+    "revision": "1d376b2962c344129e8a",
+    "url": "./static/js/runtime-main.4cbede99.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick-Morty-logo.75509f2e.png"
+  }
+]);
